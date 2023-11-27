c8d78010ab7c6ea383899d01c89541d8d40940e9
Calypso salé <calypso45000@gmail.com>
2023-11-27 12:23:30 +0100

    initial commit_tests

---
+++ tests/movies.test.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/movies/:id", () => {
+  it("should return a specific movie by ID", async () => {
+    const movies = [
+      { id: 1, title: "Movie1" },
+      { id: 2, title: "Movie2" },
+    ];
+
+    const response = await request(app).get("/api/movies/1");
+
+    expect(response.status).toBe(200);
+    expect(response.body.id).toBe(1);
+    expect(response.body.title).toBe("Movie 1");
+  });
+
+  it("should return a 404 status if tje movie is not found", async () => {
+    const response = await request(app).get("/api/movies/999");
+
+    expect(response.status).toBe(404);
+  });
+});
+
+module.exports = app;
